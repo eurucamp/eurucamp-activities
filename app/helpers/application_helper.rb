54c372dc942d97a05effc656bd9c121b5a15b035
Polarblau <polarblau@gmail.com>
2013-04-29 21:14:43 +0300

    Adding body class helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,14 @@ module ApplicationHelper
     @title << t
     @title.flatten.compact.join(separator)
   end
+
+  # Sets body classes
+  #
+  # @param [Mixed]
+  # @return [String] CSS classes
+  def body_class(c = nil)
+    @body_class ||= [controller.controller_name]
+    @body_class << c
+    @body_class.flatten.compact.uniq.join(" ")
+  end
 end
