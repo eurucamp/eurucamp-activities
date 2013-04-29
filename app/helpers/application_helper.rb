29d1e31be235dccafc816afe9ec1e49632779066
Polarblau <polarblau@gmail.com>
2013-04-29 21:13:50 +0300

    Adding title helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Sets and formats document title
+  #
+  # @param [Mixed]
+  # @param [String] Separator
+  # @return [String] Title
+  def title(t, separator = " | ")
+    @title ||= []
+    @title << t
+    @title.flatten.compact.join(separator)
+  end
 end
