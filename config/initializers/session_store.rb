ab0c07be5d228d1e1b60bcbb2e87c05319b8b587
pietia <pietia@appgrinder.pl>
2013-04-30 16:55:15 +0200

    upgraded rails to 4.0.0.rc1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Activities::Application.config.session_store :encrypted_cookie_store, key: '_activities_session'
+Activities::Application.config.session_store :cookie_store, key: '_activities_session'
