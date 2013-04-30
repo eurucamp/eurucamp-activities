2bec068d68eb3f72b4e40cb55f3603baf68649cd
Polarblau <polarblau@gmail.com>
2013-05-01 01:21:00 +0300

    Moving html helpers into gem.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,2 @@
 module ApplicationHelper
-  # Sets and formats document title
-  #
-  # @param [Mixed]
-  # @param [String] Separator
-  # @return [String] Title
-  def title(t, separator = " | ")
-    @title ||= []
-    @title << t
-    @title.flatten.compact.join(separator)
-  end
-
-  # Sets body classes
-  #
-  # @param [Mixed]
-  # @return [String] CSS classes
-  def body_class(c = nil)
-    @body_class ||= [controller.controller_name]
-    @body_class << c
-    @body_class.flatten.compact.uniq.join(" ")
-  end
 end
