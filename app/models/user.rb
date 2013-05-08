683c77e1af0db5dac2588c1c698ae872e5f9c267
pietia <pietia@appgrinder.pl>
2013-05-08 14:53:49 +0200

    upgraded simple_form, callbacks moved from c4p app ..

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,40 @@ class User < ActiveRecord::Base
 
   has_many :authentications
 
-  # validates :name, presence: true
+  validates :name, presence: true
   validates :email, presence: true
+
+  # TODO: extract to module and then to a gem / engine
+  def apply_omniauth(omniauth)
+    provider, uid, info = omniauth.values_at('provider', 'uid', 'info')
+    self.email = info['email'] if email.blank?
+    self.name  = info['name']  if name.blank?
+
+    apply_provider_handle(omniauth)
+    authentications.build(provider: provider, uid: uid)
+  end
+
+  def apply_provider_handle(omniauth)
+    provider, info = omniauth.values_at('provider', 'info')
+
+    case provider
+      when /github/
+        self.github_handle  = info['nickname'] if github_handle.blank?
+      when /twitter/
+        self.twitter_handle = info['nickname'] if twitter_handle.blank?
+    end
+  end
+
+  def password_required?
+    (authentications.empty? || password.present?) && super
+  end
+
+  def connected_with_twitter?
+    authentications.where(provider: 'twitter').first
+  end
+
+  def connected_with_github?
+    authentications.where(provider: 'github').first
+  end
+
 end
