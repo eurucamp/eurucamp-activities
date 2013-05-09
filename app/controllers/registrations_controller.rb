85df12eb8bc7c72c166feabd9a2e89cdd00f58e3
pietia <pietia@appgrinder.pl>
2013-05-09 10:25:03 +0200

    added login link

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,18 +7,18 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   private
-  def build_resource(*args)
-    super.tap do |user|
-      if user && session[:omniauth]
-        user.apply_omniauth(session[:omniauth])
-        user.valid?
+
+    def build_resource(*args)
+      super.tap do |user|
+        if user && session[:omniauth]
+          user.apply_omniauth(session[:omniauth])
+          user.valid?
+        end
       end
     end
-  end
 
-  def resource_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
+    def resource_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 end
