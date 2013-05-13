27283563fdc35883f14d4e2fc8d86459923a17ba
pietia <pietia@appgrinder.pl>
2013-05-13 14:23:39 +0200

    new hash syntax

--- db/migrate/20130513115254_create_activities.rb
+++ db/migrate/20130513115254_create_activities.rb
@@ -1,16 +1,16 @@
 class CreateActivities < ActiveRecord::Migration
   def change
     create_table :activities do |t|
-      t.string :name, :null => false, :default => ""
+      t.string :name, null: false, default: ""
       t.text :description
       t.string :place
-      t.timestamp :start_at, :null => false
+      t.timestamp :start_at, null: false
       t.integer :time_frame
       t.integer :limit_of_participants
 
       t.timestamps
     end
 
-    add_index :activities, :name, :unique => true
+    add_index :activities, :name, unique: true
   end
 end
