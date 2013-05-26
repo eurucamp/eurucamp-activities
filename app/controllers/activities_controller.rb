a1fb60e56c2d16f485b0dc81b64403781471d523
pietia <pietia@appgrinder.pl>
2013-05-26 20:33:25 +0200

    basic tests and impl. ActivitiesController#create

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,8 +1,18 @@
 class ActivitiesController < ApplicationController
-  respond_to :json, :html
+  respond_to :json, :html, :only => :index
+  respond_to :json, :only => [:create]
 
   def index
     @activities = current_event.activities
     respond_with(@activities)
   end
+
+  def create
+    sanitized_params = params.require(:activity).permit(:start_at, :name, :place, :limit_of_participants, :time_frame)
+    @activity = current_event.new_activity(sanitized_params)
+    @activity.creator = current_user
+    @activity.save
+    respond_with(@activity)
+  end
+
 end
--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -8,7 +8,25 @@ describe ActivitiesController do
     its(:status){ should == 200 }
   end
 
-  describe "#create"
+  describe "#create" do
+    context "format :json" do
+      subject { post :create, params.merge({format: :json}) }
+
+      before do
+        sign_in(FactoryGirl.create(:user))
+      end
+
+      context "valid parameters" do
+        let(:params) { {activity: {name: "Pool party", start_at: 2.days.from_now.to_s, place: "Pool", time_frame: 120 }} }
+        its(:status) { should == 201 }
+      end
+
+      context "invalid parameters" do
+        let(:params) { {activity: {x: 10}} }
+        its(:status) { should == 422 }
+      end
+    end
+  end
 
   describe "#destroy"
 
