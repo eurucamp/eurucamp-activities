fdcd7f7b5e8fc0f50f691cb5aebc7b020a07f2f7
pietia <pietia@appgrinder.pl>
2013-05-27 00:11:10 +0200

    simple implementation of update action

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,6 @@
 class ActivitiesController < ApplicationController
   respond_to :json, :html, :only => :index
-  respond_to :json, :only => [:create]
+  respond_to :json
 
   def index
     @activities = current_event.activities.decorate
@@ -8,10 +8,21 @@ class ActivitiesController < ApplicationController
   end
 
   def create
-    sanitized_params = params.require(:activity).permit(:start_at, :name, :place, :limit_of_participants, :time_frame)
     @activity = current_event.new_activity(current_user, sanitized_params)
     @activity.save
     respond_with(@activity)
   end
+
+  def update
+    @activity = current_event.activity(params[:id])
+    @activity.update_attributes(sanitized_params)
+    respond_with(@activity)
+  end
+
+  private
+
+    def sanitized_params
+      params.require(:activity).permit(:start_at, :name, :place, :limit_of_participants, :time_frame)
+    end
 
 end
