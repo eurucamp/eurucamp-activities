95f427e2e6eb24e2c3bc7d3d86921203a2da96e9
pietia <pietia@appgrinder.pl>
2013-05-26 15:59:16 +0200

    added creator_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130513115254) do
+ActiveRecord::Schema.define(version: 20130526133400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20130513115254) do
     t.integer  "limit_of_participants"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "creator_id"
   end
 
   add_index "activities", ["name"], name: "index_activities_on_name", unique: true, using: :btree
