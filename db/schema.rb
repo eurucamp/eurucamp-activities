8e8078ab1b97ddfe22745c4de509513f0e990b98
Polarblau <polarblau@gmail.com>
2013-05-27 09:47:58 +0300

    WIP. Adding participation. #36.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130526133400) do
+ActiveRecord::Schema.define(version: 20130527054241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20130526133400) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "creator_id"
+    t.integer  "participations_count",  default: 0
   end
 
   add_index "activities", ["name"], name: "index_activities_on_name", unique: true, using: :btree
@@ -36,6 +37,13 @@ ActiveRecord::Schema.define(version: 20130526133400) do
     t.string   "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "participations", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "activity_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
