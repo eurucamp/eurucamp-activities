2cba2ba49ceb6c901b6e93f305be74ff3a69a1f2
Polarblau <polarblau@gmail.com>
2013-05-27 09:47:58 +0300

    WIP. Adding participation. #36.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :authentications
+  has_many :participations, :dependent => :destroy
+  has_many :activities, through: :participations
 
   #validates :name, presence: true
   validates :email, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,26 @@
 User.transaction do |tx|
-  user = User.new(
+  creator = User.new(
     email: "johnny@cash.com",
     name: "Johnny Cash"
   )
-  user.password=user.password_confirmation="qweqweqwe"
-  user.save!
+  creator.password = creator.password_confirmation = "qweqweqwe"
+  creator.save!
 
-  Activity.create!(
+  participant = User.new(
+    email: "johnny@begood.com",
+    name: "Johnny Begood"
+  )
+  participant.password = participant.password_confirmation = "qweqweqwe"
+  participant.save!
+
+  activity = Activity.new(
     name: "Party!",
     start_at: 1.day.from_now.to_time,
     place: "Pool",
     time_frame: 200,
-    creator: user
+    limit_of_participants: 2,
+    creator: creator
   )
+  activity.participants << participant
+  activity.save!
 end
