1e9e2f7931731150b1b7566fa8fe926163b20e5d
pietia <pietia@appgrinder.pl>
2013-05-28 22:33:37 +0200

    added specs for validations in Participation model

--- spec/models/participation_spec.rb
+++ spec/models/participation_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe Participation do
+
+  describe "validations" do
+
+    it { should     accept_values_for(:user_id, 1 ) }
+    it { should_not accept_values_for(:user_id, "", nil) }
+
+    it { should     accept_values_for(:activity_id, 1 ) }
+    it { should_not accept_values_for(:activity_id, "", nil) }
+
+    specify { FactoryGirl.create(:participation).clone.save! }.to raise(ActiveRecord::RecordInvalid)
+
+  end
+
 end
