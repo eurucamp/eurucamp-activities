e2e182687baf024162cf9fdd05715e2ca113f3af
Polarblau <polarblau@gmail.com>
2013-05-29 20:13:50 +0300

    Reloading activity.

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -4,13 +4,13 @@ class ParticipationsController < ApplicationController
   def create
     participation = current_activity.new_participation(current_user)
     participation.save
-    respond_with(current_activity, participation)
+    respond_with(current_activity.reload, participation)
   end
 
   def destroy
     participation = current_activity.participation(current_user)
     participation.destroy if participation && participation.participant == current_user #TODO
-    respond_with(current_activity, participation)
+    respond_with(current_activity.reload, participation)
   end
 
   private
