65cec7cbc7659afd68732b1fc95ac4ddc52baa7f
pietia <pietia@appgrinder.pl>
2013-06-06 13:09:30 +0200

    test basic settings

--- spec/lib/settings_spec.rb
+++ spec/lib/settings_spec.rb
@@ -5,4 +5,16 @@ describe "Settings" do
   subject { Settings }
 
   its(:host) { should_not be_blank }
+  its(:event) { should_not be_blank }
+
+  describe "#event" do
+
+    subject { Settings.event }
+
+    its(:name)       { should_not be_blank }
+    its(:start_time) { should be_a_kind_of(Time) }
+    its(:end_time)   { should be_a_kind_of(Time) }
+
+  end
+
 end
