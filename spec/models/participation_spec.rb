aec61c1fc405c4a61be7dd74f2de582afb363fec
pietia <pietia@appgrinder.pl>
2013-06-08 12:56:19 +0200

    cleanups

--- spec/models/participation_spec.rb
+++ spec/models/participation_spec.rb
@@ -4,13 +4,13 @@ describe Participation do
 
   describe "validations" do
 
+    specify { expect { FactoryGirl.create(:participation).dup.save! }.to raise_exception(ActiveRecord::RecordInvalid) }
+
     it { should     accept_values_for(:user_id, 1 ) }
     it { should_not accept_values_for(:user_id, "", nil) }
 
     it { should     accept_values_for(:activity_id, 1 ) }
     it { should_not accept_values_for(:activity_id, "", nil) }
-
-    specify { expect { FactoryGirl.create(:participation).dup.save! }.to raise_exception(ActiveRecord::RecordInvalid) }
 
   end
 
