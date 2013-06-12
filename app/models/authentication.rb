543534e5cffb612b55e2a81fbac6f8d540b9cadf
pietia <pietia@appgrinder.pl>
2013-06-12 12:36:58 +0200

    more specs for models

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,3 +1,8 @@
 class Authentication < ActiveRecord::Base
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :provider, presence: true, uniqueness: { scope: [:user_id] }
+  validates :uid, presence: true
+
 end
