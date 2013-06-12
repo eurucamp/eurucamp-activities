bde82c19a0b55838a2801bdc76cc24f07700ffab
pietia <pietia@appgrinder.pl>
2013-06-12 12:40:28 +0200

    cleanups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
