43acb54a784f61c8254852e0779a06cee0961c27
pietia <pietia@appgrinder.pl>
2013-06-21 15:14:17 +0200

    more refactoring in controller specs

--- spec/controllers/authentications_controller_spec.rb
+++ spec/controllers/authentications_controller_spec.rb
@@ -2,8 +2,14 @@ require "spec_helper"
 
 describe AuthenticationsController do
 
+  let(:current_user) { mock_model(User) }
+  let(:authentications) { mock(:authentications) }
+  let(:authentication) { mock(:authentication) }
+  let(:authentication_id) { "1" }
+
   before do
     @request.env["devise.mapping"] = Devise.mappings[:user]
+    current_user.stub!(:authentications).and_return(authentications)
   end
 
   describe '#create' do
@@ -27,7 +33,10 @@ describe AuthenticationsController do
       let(:omniauth_data) { { provider: 'none',  uuid: 'none' } }
 
       before do
-        sign_in FactoryGirl.create(:user)
+        sign_in(current_user)
+        authentications.should_receive(:find_or_create_by).with({provider: nil, uid: nil})
+        current_user.should_receive(:apply_provider_handle).with(omniauth_data)
+        current_user.should_receive(:save).and_return(true)
       end
 
       context "when HTTP_REFERER set" do
@@ -39,25 +48,24 @@ describe AuthenticationsController do
       end
 
       context "when HTTP_REFERER not set" do
-         it { should redirect_to edit_user_registration_path }
+
+        it { should redirect_to edit_user_registration_path }
       end
     end
 
   end
 
   describe "#delete" do
-    subject(:logout_action) { delete :destroy, id: authentication.id }
-    let(:authentication) { FactoryGirl.create(:authentication) }
+    subject(:logout_action) { delete :destroy, id: authentication_id }
 
     before do
-      sign_in authentication.user
+      sign_in(current_user)
+      authentications.should_receive(:find).with(authentication_id).and_return(authentication)
+      should_authorize(:destroy, authentication)
+      authentication.should_receive(:destroy).and_return(true)
     end
 
     it { should redirect_to authentications_path }
-
-    specify do
-      expect { logout_action }.to change(Authentication, :count).by(-1)
-    end
   end
 
 end
