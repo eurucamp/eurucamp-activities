2b414c49dd6eb170cff5ffa84eda90f528cbfc2b
pietia <pietia@appgrinder.pl>
2013-08-08 14:57:53 +0200

    add smtp configuration

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -86,3 +86,13 @@ Activities::Application.configure do
                         :sender_address => %{"Exception Notifier" <#{Settings.errors.from}>},
                         :exception_recipients => Settings.errors.to
 end
+
+ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+}
