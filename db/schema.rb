7bcaa25a70a640823f6b81622cc284f5b864007d
Alex Coles <alex@alexbcoles.com>
2015-06-20 20:54:52 +0200

    Add authentication_token to users table
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130608105751) do
+ActiveRecord::Schema.define(version: 20150620185319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20130608105751) do
     t.string   "name"
     t.string   "twitter_handle"
     t.string   "github_handle"
+    t.string   "authentication_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
