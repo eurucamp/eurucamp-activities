8c86820621d71adad06e022f9ec08a2006914a91
Alex Coles <alex@alexbcoles.com>
2015-06-20 20:57:56 +0200

    Add hooks for token generation to User
    Based on server setup guide:
    https://github.com/simplabs/ember-simple-auth/tree/master/packages/ember-simple-auth-devise
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,14 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true
 
+  before_save :ensure_authentication_token
+
+  def ensure_authentication_token
+    if authentication_token.blank?
+      self.authentication_token = generate_authentication_token
+    end
+  end
+
   # TODO: extract to module and then to a gem / engine
   def apply_omniauth(omniauth)
     provider, uid, info = omniauth.values_at('provider', 'uid', 'info')
@@ -64,6 +72,13 @@ class User < ActiveRecord::Base
   end
 
   private
+
+    def generate_authentication_token
+      loop do
+        token = Devise.friendly_token
+        break token unless User.where(authentication_token: token).first
+      end
+    end
 
     def any_oauth_connected?
       authentications.any?
