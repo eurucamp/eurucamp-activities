5dc6b6a14f7d61cb90e1c00b3eccc1ab0c0bb04e
Alex Coles <alex@alexbcoles.com>
2015-06-20 10:35:56 +0200

    Add body CSS classes for given routes
    /HT https://gist.github.com/henriquea/b32cdc7a0067addef746
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- frontend/app/routes/activities.js
+++ frontend/app/routes/activities.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.findAll('activity');
+  },
+  activate: function() {
+    Ember.$('body').addClass('activities index');
+  },
+  deactivate: function() {
+    Ember.$('body').removeClass('activities index');
   }
 });
--- frontend/app/routes/activity.js
+++ frontend/app/routes/activity.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('activity', params.activity_id);
+  },
+  activate: function() {
+    Ember.$('body').addClass('activities show');
+  },
+  deactivate: function() {
+    Ember.$('body').removeClass('activities show');
   }
 });
