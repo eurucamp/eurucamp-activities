bb6e8b59a53e1efb1d09d4c47c10b3e5cffbc0a3
Alex Coles <alex@alexbcoles.com>
2015-06-21 12:29:09 +0200

    Add body CSS classes for login route
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- frontend/app/routes/login.js
+++ frontend/app/routes/login.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'simple-auth/mixins/unauthenticated-route-mixin';
 
 export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  activate: function() {
+    Ember.$('body').addClass('user sessions new');
+  },
+  deactivate: function() {
+    Ember.$('body').removeClass('user sessions new');
+  }
 });
