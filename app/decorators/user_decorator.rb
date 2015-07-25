898999f2863660a4fa89dc3d3b57eda6e503074e
Martin Luder <maser@winterfell.ch>
2015-07-25 23:41:57 +0200

    Add option to hide participation in activities
    Not everyone wants to show publicly where and who they are. So we
    provide an option to show a users participations as anonymous.

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,9 +1,13 @@
 class UserDecorator < Draper::Decorator
   def name
+    return 'Anonymous' unless object.show_participation
     object.name
   end
 
+  DEFAULT_AVATAR = 'http://www.gravatar.com/avatar/00000000000000000000000000000000.png?s=%{size}'
+
   def avatar_url(size)
+    return DEFAULT_AVATAR % { size: size } unless object.show_participation
     h.avatar_url(object, size)
   end
 end
