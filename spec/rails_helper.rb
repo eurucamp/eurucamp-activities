2a465035b697db82dd23629d8ac416587a3253c2
Martin Luder <maser@winterfell.ch>
2015-07-26 19:46:14 +0200

    Set up Warden test mode for feature tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,6 +63,13 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
 
   config.include Devise::TestHelpers, type: :controller
+  config.include Warden::Test::Helpers
+  config.before :suite do
+    Warden.test_mode!
+  end
+  config.after :each do
+    Warden.test_reset!
+  end
   config.include ControllerHelpers, type: :controller
   config.include JsonSpec::Helpers, type: :controller
 
